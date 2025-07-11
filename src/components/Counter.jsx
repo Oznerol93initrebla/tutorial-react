68420f0e13494d59e01a9308097e74605c02a73e
Lorenzo Albertini <lorialbertini10@gmail.com>
2025-07-11 19:20:55 +0200

    logica di aggiornamento di setCount

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 
-export const Counter =() => {
+export const Counter = () => {
     const [count, setCount] = useState(0);
     return (
         <div>
         <h2>Counter: {count}</h2>
-        <button>Incrementa</button>
+        <button onClick={()=>setCount(count + 1)}>Incrementa</button> {/* aggiunta logica di aggiornamento del counter */}
     </div>
     )
 }
