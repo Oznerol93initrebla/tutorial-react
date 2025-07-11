3b13205ac024d4ffcb644582101b3a1d130e03ae
Lorenzo Albertini <lorialbertini10@gmail.com>
2025-07-11 19:33:20 +0200

    aggiornamento corretto di aggiunta props chiamata start

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
-export const Counter = (start) => {
+export const Counter = ({start}) => {
     const [count, setCount] = useState(start);
     return (
         <div>
-        <h2>Counter: {count}</h2>
-        <button onClick={()=>setCount(count + 1)}>Incrementa</button> {/* aggiunta logica di aggiornamento del counter */}
-    </div>
+            <h2>Counter: {count}</h2>
+            <button onClick={()=>setCount(count + 1)}>Incrementa</button> {/* aggiunta logica di aggiornamento del counter */}
+        </div>
     )
 }
 
