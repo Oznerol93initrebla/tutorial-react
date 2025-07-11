ec761af040ecf3f4ce1c37d2d69e2dca30110f5a
Lorenzo Albertini <lorialbertini10@gmail.com>
2025-07-11 21:38:19 +0200

    aggiunta altre voci utenti da visualizzare

--- src/components/RandomUser.jsx
+++ src/components/RandomUser.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export function RandomUser() {
     const [user, setUser] = useState(null)
@@ -7,18 +6,19 @@ export function RandomUser() {
     useEffect(() => {
         //faccio qualcosa
         fetch('https://randomuser.me/api/')
-        .then((res)=>res.json()) //risultato di tipo json
+        .then((res) => res.json()) //risultato di tipo json
         .then((data) => setUser(data.results[0])) //questa la struttura dell'API di riferimento prendendo il primo array 
-    }
-    ,[])
+    } ,[])
     return (
         <div>
             <h2>Utente Casuale</h2>
             {
                 user ? // vogliamo stampare il nome dell'utente preso dal comando fetch
-                <>
-                <p>{user.name.first}</p> 
-                </>
+                <div>
+                    <img src={user.picture.large} alt="user img" />
+                    <h3>{user.name.first} {user.name.last}</h3> 
+                    <p>Email: {user.email}</p>
+                </div>
                 :
                 <p>Caricamento...</p>
             }
